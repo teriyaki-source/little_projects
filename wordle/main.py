91ee52caba542a114ad952e8ffdcc45fcedaec99
teriyaki-source <Luke.marinaki@gmail.com>
2024-04-05 21:06:13 +1100

    bootleg wordle version 1.1.1

--- wordle/main.py
+++ wordle/main.py
@@ -10,18 +10,26 @@ def clean():
   else:
     _ = os.system('clear')
 
+def print_board(guesses, states):
+  clean()
+  for i in range(len(guesses)):
+    print(guesses[i])
+    print(states[i])
+
 def check_word(answer, attempt):
-    result = ["x","x","x","x","x"]
-    for i in range(len(attempt)):
-        if attempt[i] in answer:
-            if attempt[i] == answer[i]:
-                result[i] = "y"
-            else:
-                result[i] = "~"
-        else:
-            result[i] = "x"
-    result = ''.join(result)
-    return result
+  # todo: make check more robsut to account for double letters out of order/guessing doubles when 
+  # not required. e.g. answer is ghost, guessing goops returns y~yn~ but should be ynyn~
+  result = ["x","x","x","x","x"]
+  for i in range(len(attempt)):
+    if attempt[i] in answer[i:]:
+      if attempt[i] == answer[i]:
+        result[i] = "y"
+      else:
+        result[i] = "~"
+    else:
+      result[i] = "x"
+  result = ''.join(result)
+  return result
 
 guesses = ["-----","-----","-----","-----","-----","-----"]
 states = ["xxxxx","xxxxx","xxxxx","xxxxx","xxxxx","xxxxx"]
@@ -30,7 +38,7 @@ attempt = ""
 f = open("wordle-La.txt", "r")
 can_be = f.readlines()
 for i in range(len(can_be)):
-    can_be[i] = can_be[i].strip()
+  can_be[i] = can_be[i].strip()
 f.close()   
 
 f = open("wordle-Ta.txt", "r")
@@ -40,6 +48,10 @@ for i in range(len(can_guess)):
 f.close()
 answer = can_be[random.randint(0,len(can_be)-1)]
 library = can_be + can_guess
+
+
+# print(answer)
+# game start
 
 print("** Bootleg Wordle **")
 print("By Luke, for Chiara")
@@ -55,13 +67,11 @@ while count < 6:
       print("Needs to be a 5 letter word!")
     elif attempt not in library:
       print("Not in the word list")
+    print_board(guesses, states)
     attempt = input("Guess: ")
   guesses[count-1] = attempt
   states[count-1] = check_word(answer, attempt)
-  clean()
-  for i in range(len(guesses)):
-    print(guesses[i])
-    print(states[i])
+  print_board(guesses, states)
   if attempt == answer:
     break
 
@@ -70,5 +80,3 @@ if attempt == answer:
   print("Bootleg Wordle in {num:d}".format(num = count))
 else:
   print("Better luck next time")
-
-
