6c9ebc31d74f14f0020eac4cb8b2ea129b091154
teriyaki-source <Luke.marinaki@gmail.com>
2024-04-08 17:43:12 +1000

    slightly improved starting view, added available letters

--- wordle/main.py
+++ wordle/main.py
@@ -2,7 +2,6 @@ import random
 import os
 
 # define colours for output
-blank = "\U00002800"
 green = "\U0001F7E9"
 white = "\U00002B1C"
 yellow = "\U0001F7E8"
@@ -27,33 +26,32 @@ def clean():
   # For Windows
   if os.name == 'nt':
     _ = os.system('cls')
-  
   # For macOS and Linux
   else:
     _ = os.system('clear')
 
-def print_board(attempt, guesses, states):
+def print_board(attempt, guesses, states, letters):
   clean()
   if len(attempt) != 5:
     print("Needs to be a 5 letter word!")
   elif attempt not in library:
-    print("Not in the word list")
+    print("{word:s} not in the word list".format(word = attempt))
+  
+  print("Available Letters:")
+  for i in letters:
+    print(i, end = " ")
+  print("\n")
+
   for i in range(len(guesses)):
-    for j in range(len(guesses[i])):
-      print("{0: <2.5}".format(guesses[i][j]),end ="")
-    print()
-    if states[i][0] == "-":
-      for j in range(len(states[i])):
-        print("{0: <2}".format(states[i][j]),end = "")
+    if guesses[i][0] != " ":
+      for j in range(len(guesses[i])):
+        print("{0: <2.5}".format(guesses[i][j]),end ="")
       print()
-    else:
+    if states[i][0] != " ":
       print(states[i])
 
-def check_word(answer, attempt):
+def check_word(answer, attempt, letters):
   result = [white, white, white, white, white]
-  return check_double_letter(result, answer, attempt)
-
-def check_double_letter(result, answer,attempt):
   for i in range(len(attempt)):
     no_attempt = attempt.count(attempt[i])
     no_answer = answer.count(attempt[i])
@@ -65,40 +63,40 @@ def check_double_letter(result, answer,attempt):
           result[i] = yellow
       else:
         result[i] = white
+        if attempt[i] in letters:
+          letters.remove(attempt[i])
     else:
       if attempt[i] == answer[i]:
         result[i] = green
       else:
         result[i] = white
+        if attempt[i] in letters:
+          letters.remove(attempt[i])
   return "".join(result)
+  
 
-guesses =   ["-----"]*6
-states =    ["-----"]*6
+guesses = [" "]*6
+states = [" "]*6
 
 count = 0
 attempt = ""
-
 can_be, can_guess, answer, library = get_data()
+available_letters = list(map(chr, range(97, 123)))
 
-# print(answer)
 # game start
-
+clean()
 print("** Bootleg Wordle **")
 print("By Luke, for Chiara")
-print("Game Begin:")
-
-# for i in guesses:
-#   print(i)
 
 while count < 6:
   count += 1
   attempt = input("Guess: ")
   while len(attempt) != 5 or attempt not in library:
-    print_board(attempt, guesses, states)
+    print_board(attempt, guesses, states, available_letters)
     attempt = input("Guess: ")
   guesses[count-1] = attempt
-  states[count-1] = check_word(answer, attempt)
-  print_board(attempt, guesses, states)
+  states[count-1] = check_word(answer, attempt, available_letters)
+  print_board(attempt, guesses, states, available_letters)
   if attempt == answer:
     break
 
